f4ac2c538ce319721073277877830052cad5c6ca
PouchyCorp <pouchy.l.acarien@gmail.com>
2025-03-19 16:57:47 +0100

    FINITION VRAIMENT FINALE

--- sources/core/logic.py
+++ sources/core/logic.py
@@ -359,7 +359,8 @@ class Game:
                 if not hasattr(placeable, 'no_interaction'):
                     self.popups.append(InfoPopup(placeable.name))
 
-                    
+                       
+                       
 
     def handle_door_down_interaction(self, placeable):
         if self.unlock_manager.is_floor_unlocked(self.current_room.num-1): # Check if the next floor is unlocked
